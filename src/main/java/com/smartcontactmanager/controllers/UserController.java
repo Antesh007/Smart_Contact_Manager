c0e98692d9b3224864469f3e8bf23cbb734ee9f2
Antesh007 <anteshmukherjee007@gmail.com>
2024-05-30 12:25:08 +0530

    Added the Add Contact Page and Spring Security Configuration
    Added the Add Contact Page and Spring Security Configuration

--- src/main/java/com/smartcontactmanager/controllers/UserController.java
+++ src/main/java/com/smartcontactmanager/controllers/UserController.java
@@ -1,14 +1,33 @@
 package com.smartcontactmanager.controllers;
 
+
+
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ssl.SslProperties.Bundles.Watch.File;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.smartcontactmanager.entities.Contact;
 import com.smartcontactmanager.entities.User;
 import com.smartcontactmanager.repository.UserRepository;
+
+import jakarta.persistence.criteria.Path;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 @Controller
 @RequestMapping("/user")
@@ -17,9 +36,9 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@RequestMapping("/welcome")
-	public String user_Dashboard(Model model,Principal principal)
-	{
+	@ModelAttribute
+	public void addModelDetails(Model model, Principal principal) {
+		
 		String emailString = principal.getName();
 		System.out.println("Username"+emailString);
 		
@@ -27,8 +46,55 @@ public class UserController {
 		System.out.println("User Details"+userByUserName.toString());
 		
 		model.addAttribute("user",userByUserName);
-		
+	}
+	
+	@RequestMapping("/welcome")
+	public String user_Dashboard(Model model,Principal principal)
+	{
+		model.addAttribute("title","User DashBoard");
 		return "normal/user_dashboard";
+	}
+	
+	@GetMapping("/add-contact")
+	public String addContactForm(Model model)
+	{
+		model.addAttribute("title","Add Contact");
+		model.addAttribute("contact",new Contact());
+		return "normal/add_contact_form";
+	}
+	
+	@PostMapping("/process-contact")
+	public String processContactForm(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file, Principal principal)
+	{
+		try {
+		String userNameString = principal.getName();
+		User user = this.userRepository.getUserByUserName(userNameString);
+		
+		contact.setUser(user);
+		if (file.isEmpty()) {
+			System.out.println("File is Empty");
+		} else {
+			contact.setImage(file.getOriginalFilename());
+			
+			String saveFiles = new ClassPathResource("static/image").getFile().getAbsolutePath();
+			
+			java.nio.file.Path path =  Paths.get(saveFiles , file.getOriginalFilename());
+			try {
+			//	OutputStream in = new ByteArrayInputStream(file.getBytes());
+			file.transferTo(path);
+			} catch (Exception e) {
+				System.out.println("Ërror in file Uploading" +e);
+			}
+		}
+		user.getContacts().add(contact);
+		
+		this.userRepository.save(user);
+		System.out.println("Contact successfully saved");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "normal/add_contact_form";
 	}
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,3 +2,15 @@
  * 
  */
 console.log("This is working Fine");
+
+const toggleSidebar = () => {
+	if($(".sidebar").is(":visible")) {
+		//true
+		$(".sidebar").css("display", "none");
+		$(".content").css("margin-left", "0%");
+	} else {
+		//false
+		$(".sidebar").css("display", "block");
+		$(".content").css("margin-left", "20%");
+	}
+}
